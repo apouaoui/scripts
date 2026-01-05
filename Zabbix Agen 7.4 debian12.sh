90632ca2db6af723491a6827d0c7252acd530cab
Arthur Passos Ouaoui <135644554+apouaoui@users.noreply.github.com>
2026-01-05 09:35:06 -0300

    Add script to install Zabbix Agent 7.4 on Debian 12

---
+++ Zabbix Agen 7.4 debian12.sh
@@ -0,0 +1,7 @@
+wget https://repo.zabbix.com/zabbix/7.4/release/debian/pool/main/z/zabbix-release/zabbix-release_latest_7.4+debian12_all.deb
+dpkg -i zabbix-release_latest_7.4+debian12_all.deb
+apt update
+apt install zabbix-agent -y
+sudo sed -i 's/^Server=127.0.0.1/Server=10.0.10.15/' /etc/zabbix/zabbix_agentd.conf
+systemctl restart zabbix-agent
+systemctl enable zabbix-agent
